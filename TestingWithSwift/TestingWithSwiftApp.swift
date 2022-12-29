f0576fe8e943973d3930932b8813d86920078997
Jordan Calhoun <jordancalhoun@gmail.com>
2022-12-29 11:06:30 -0500

    Initial Commit

---
+++ TestingWithSwift/TestingWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingWithSwiftApp.swift
+//  TestingWithSwift
+//
+//  Created by Jordan Calhoun on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
